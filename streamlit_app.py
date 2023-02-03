94e2159a57f86933f85a57897be096cbb5b8f197
lucianotejada1995 <124300690+lucianotejada1995@users.noreply.github.com>
2023-02-03 14:34:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-
 streamlit.header('Breakfast Menu')
-
-streamlit.text('2 eggs')
+streamlit.text('🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
