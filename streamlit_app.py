61a026af00272fa1f12684da68462549e0af0794
lucianotejada1995 <124300690+lucianotejada1995@users.noreply.github.com>
2023-02-03 14:35:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('My own menu')
